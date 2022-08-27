fc1788cae01381d1b21e8578526da94b1a1c3a67
Brian Choi <brianchoix31@gmail.com>
2022-08-27 03:26:31 -0400

    able to fetch data from api into json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-var APIKey = '02469bc88dce331a39026a7921fdbecc';
-var city;
-var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
-fetch(queryURL);
+let searchButton = document.querySelector('.btn');
+
+searchButton.addEventListener('click', () =>  {
+    userCityInput = document.getElementById('user-input').value;
+    console.log(userCityInput);
+    fetchWeather(userCityInput);
+});
+
+async function fetchWeather () {
+    let apiKey = '02469bc88dce331a39026a7921fdbecc';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${userCityInput}&units=imperial&appid=${apiKey}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+}
+
+fetchWeather();
